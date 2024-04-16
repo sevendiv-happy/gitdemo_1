bba6b0471877562cbcf25423aa87b22dac691f53
sunbo <1054746991@qq.com>
2024-04-16 23:59:04 +0800

    第三次修改

--- student.java
+++ student.java
@@ -2,5 +2,6 @@ public class student {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("nihao");
+        System.out.println("sjdjfjsjdf");
     }
 }
